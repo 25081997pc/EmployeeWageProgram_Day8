0be702b375428675cbcf28d5877f5f23e4d64015
25081997pc <chavanpooja2508@gmail.com>
2022-05-29 15:46:45 +0530

    Checking Employee Present or Not

---
+++ EmployeeCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgramDay8
+{
+    public class EmployeeCheck
+    {
+        public void CheckEmployeePresentOrNot()
+        {
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empChk = random.Next(2);
+
+            if (empChk == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+
+}
