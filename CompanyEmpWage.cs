99ae0028e09e357a679b45cbfd1a10c7283e3076
25081997pc <chavanpooja2508@gmail.com>
2022-05-31 19:28:06 +0530

    Creating CompanyEmpWage Class

---
+++ CompanyEmpWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgramDay8
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHr;
+        public int NumOfWorkingDays;
+        public int MaxHrsPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(string company, int empRatePerHr, int NumofWorkingDays, int MaxHrsPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHr = empRatePerHr;
+            this.NumOfWorkingDays = NumofWorkingDays;
+            this.MaxHrsPerMonth = MaxHrsPerMonth;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        public string tostring()
+        {
+            return "Total Emp Wage for company: " + this.company + " is : " + this.totalEmpWage;
+                
+        
+        
+        }
+    }
+}
